df21d7e106d4196f8b44c6204ef7503c058a762c
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2024-02-08 14:44:20 +0300

    Передвижение игрока

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,9 +3,21 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+func _set_animation(action: String):
+	if Input.is_action_pressed(action):
+		$AnimatedSprite2D.animation = action
+		$AnimatedSprite2D.play()
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED
+	if direction.length() == 0:
+		$AnimatedSprite2D.animation = 'idle'
+		$AnimatedSprite2D.stop()
+	else:
+		_set_animation('left')
+		_set_animation('right')
+		_set_animation('up')
+		_set_animation('down')
 
 	move_and_slide()

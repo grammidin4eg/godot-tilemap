b47f508de7c3b49f8d8efcba5c4a770da5ac905a
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2024-02-21 14:13:48 +0300

    выводим предметы в инвентаре

--- inventory/inv_panel.gd
+++ inventory/inv_panel.gd
@@ -1,7 +1,15 @@
 extends Panel
 
+@onready var inventory: Inventory = preload("res://inventory/inventory_res.tres")
+@onready var slots: Array = $GridContainer.get_children()
+
+func update():
+	for i in range(min(inventory.items.size(), slots.size())):
+		slots[i].update(inventory.items[i])
+
 func _ready():
 	visible = false
+	update()
 
 func open():
 	visible = true
